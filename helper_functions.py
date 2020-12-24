81c03d83242f3186d32388eb284b25dadd2de643
Ali <alijfri99@gmail.com>
2020-12-24 13:51:28 +0330

    Changed the encoder to OneHotEncoder

--- helper_functions.py
+++ helper_functions.py
@@ -1,10 +1,10 @@
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 
 def prepare_column(dataframe, column_name, is_text):
     dataframe[column_name].fillna(dataframe[column_name].mode()[0], inplace=True)  # handle the missing values
     if is_text:  # then encode the column
-        enc = OrdinalEncoder()
+        enc = OneHotEncoder(sparse=False)
         enc.fit(dataframe[[column_name]])
         dataframe[column_name] = enc.transform(dataframe[[column_name]])
 
